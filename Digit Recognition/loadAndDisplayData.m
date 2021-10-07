1772b31c969777a4e8481a385519b9d870011b00
Kartikey Jaiswal <52877433+kjkartikey@users.noreply.github.com>
2021-10-07 23:18:24 +0530

    Add files via upload

---
+++ Digit Recognition/loadAndDisplayData.m
@@ -0,0 +1,7 @@
+load('data.mat');
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+displayData(X(sel, :));
---
+++ Digit Recognition/randInitializeWeights.m
@@ -0,0 +1,8 @@
+function W = randInitializeWeights(L_in, L_out)
+  %RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+  %incoming connections and L_out outgoing connections
+  W = zeros(L_out, 1 + L_in);
+  
+  epsilon_init = sqrt(6)/(sqrt(L_in)+sqrt(L_out));
+  W = - epsilon_init + rand(L_out, 1 + L_in) * 2 * epsilon_init ;
+end
